e56654d852ec37f6cf24bb1ac5f588a63d13e2aa
Usuario <Usuario@CPD03-M17>
2024-11-04 15:09:45 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras/Helpers/SQLiteDataBaseHelpers.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MauiAppMinhasCompras.Models;
+using SQLite;
+
+namespace MauiAppMinhasCompras.Helpers
+{
+    public class SQLiteDataBaseHelpers
+    {
+        readonly SQLiteAsyncConnection _conn;
+
+        public SQLiteDataBaseHelpers(string path)
+        {
+            _conn = new SQLiteAsyncConnection(path); 
+            _conn.CreateTableAsync<Produto>().Wait();
+        }
+
+        public Task<int> Insert(Produto p)
+        {
+            return _conn.InsertAsync(p);
+        }
+
+        public Task<List<Produto>> Update(Produto p)
+        {
+            string sql = "UPDATE produto SET Descricao = ?, Quantidade = ?, " +
+                "Quantidade = ?, Preco = ? WHERE Id = ?";
+
+            return _conn.QueryAsync<Produto>(sql, p.Descricao, p.Quantidade, p.Preco, p.Id);
+        }
+    }
+}
---
+++ MauiAppMinhasCompras/Models/Produto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace MauiAppMinhasCompras.Models
+{
+    public class Produto
+    {
+        string _descricao;
+
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Descricao
+        {
+            get => _descricao;
+            set
+            {
+                if (value == null)
+                {
+                    throw new Exception("Por favor, preencha a descrição");
+                }
+
+                _descricao = value;
+            }
+        }
+        public double Quantidade { get; set; }
+        public double Preco {  get; set; }
+        public double Total { get; set; }
+    }//Fecha classe
+}//Fecha NameSpace
